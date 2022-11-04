4a797996685c3ad95a326a41672859380604fb69
Pertti <pentti.palojarvi@protonmail.com>
2022-11-04 16:34:30 +0100

    Figures

--- process.py
+++ process.py
@@ -14,8 +14,11 @@ for filename in os.scandir("tiff_images"):
         numpy.set_printoptions(threshold=sys.maxsize,linewidth=230)
         # to find dense submatrices
         neighborsum = convolve2d(imarray,numpy.ones((3,3),dtype=int),'same') - imarray
+        print(neighborsum.sum(axis=1))
         print(any(i > 512 for i in neighborsum.sum(axis=1)))
         print(any(i == 0 for i in neighborsum.sum(axis=1)))
+        plt.bar(range(len(neighborsum.sum(axis=1))), neighborsum.sum(axis=1))
+        plt.show()
         bsr_matr = sparse.bsr_matrix(imarray.astype(int))
         bsr_matr.maxprint = bsr_matr.count_nonzero()
         f = open("processed/"+filename.name+"_bsr.txt", "w")
